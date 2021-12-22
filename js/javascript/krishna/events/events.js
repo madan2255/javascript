bd8740f573c72eee6d3747a7267c2e58ee718011
madanreddy <96063006+madan2255@users.noreply.github.com>
2021-12-22 10:05:46 +0530

    Add files via upload

---
+++ js/javascript/krishna/events/events.js
@@ -0,0 +1,24 @@
+function maddy()
+{
+    document.getElementById('one').style.backgroundColor="blue";
+}
+function chandu()
+{
+    document.getElementById('two').style.backgroundColor="green";
+}
+function teja()
+{
+    document.getElementById('three').style.backgroundColor="blue";
+}
+function sujay()
+{
+    document.getElementById('four').style.backgroundColor="blue";
+}
+function rishik()
+{
+    document.getElementById('five').style.backgroundColor="blue";
+}
+function sandhya()
+{
+    document.getElementById('six').style.backgroundColor="blue";
+}
